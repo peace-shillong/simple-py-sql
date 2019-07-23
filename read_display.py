076c66cd447a78668a50c8ff53bc25b644ab1492
KYRSHANLANG R DKHAR <33800152+peace-shillong@users.noreply.github.com>
2019-07-24 00:16:04 +0530

    Update read_display.py

--- read_display.py
+++ read_display.py
@@ -10,13 +10,16 @@ try:
    cursor = mySQLconnection .cursor()
    cursor.execute(sql_select_Query)
    records = cursor.fetchall()
-   print("Total number of rows in python_developers is - ", cursor.rowcount)
+   print("Total number of rows in employee is - ", cursor.rowcount)
    print ("Printing each row's column values i.e.  employee record")
    for row in records:
-       print("Id = ", row[0], )
+       print("EMP Id = ", row[0], )
        print("Name = ", row[1])
-       print("JoiningDate  = ", row[2])
-       print("Salary  = ", row[3], "\n")
+       print("Designation  = ", row[2])
+       print("Qualification  = ", row[3])
+       print("DOB  = ", row[4])
+       print("Address  = ", row[5])
+       print("Gender  = ", row[6],"\n")      
    cursor.close()
    
 except Error as e :
