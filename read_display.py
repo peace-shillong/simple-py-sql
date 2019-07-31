c8fc08d459f1e8119ddaeb3466ef5bfeb194ce98
KYRSHANLANG R DKHAR <33800152+peace-shillong@users.noreply.github.com>
2019-07-31 11:52:06 +0530

    Update read_display.py

--- read_display.py
+++ read_display.py
@@ -1,35 +1,27 @@
 #Read and Display Employee Details from employee table
-import mysql.connector
-from mysql.connector import Error
-try:
-   mycon = mysql.connector.connect(host='localhost',
+import mysql.connector as sqltor
+mycon = sqltor.connect(host='localhost',
                              database='company',
                              user='root',
                              password='')
-   sql_select_Query = "select * from employee"
-   cursor = mycon .cursor()
-   cursor.execute(sql_select_Query)
-   records = cursor.fetchall()
-   print("Total number of rows in employee is - ", cursor.rowcount)
-   print ("Printing each row's column values i.e.  employee record")
-   if cursor.rowcount > 0:
-      for row in records:
-       print("EMP Id = ", row[0])
-       print("Name = ", row[1])
-       print("Designation  = ", row[2])
-       print("Qualification  = ", row[3])
-       print("DOB  = ", row[4])
-       print("Address  = ", row[5])
-       print("Gender  = ", row[6],"\n")
-       #Display Tabular form
-       # for row in records:
-       #    print(row)
-   cursor.close()
+if mycon.is_connected():
+   print('Successfully Connected')
+      
+sql_select_Query = "select * from employee"
    
-except Error as e :
-    print ("Error while connecting to MySQL", e)
-finally:
+cursor = mycon.cursor()
+cursor.execute(sql_select_Query)
+   
+records = cursor.fetchall()
+print("Total number of rows in employee is - ", cursor.rowcount)
+   
+print ("Printing each row's column values i.e.  employee record")
+
+for row in records:
+   print(row)
+cursor.close()
+
     #closing database connection.
-    if(mycon .is_connected()):
-        mycon.close()
-        print("MySQL connection is closed")
+if(mycon .is_connected()):
+   mycon.close()
+   print("MySQL connection is closed")
